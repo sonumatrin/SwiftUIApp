ebd954e8fd645d172975913b8a23d9de14caae5d
Sonu Martin <sonu.martin@claysys.net>
2023-09-02 21:24:42 -0400

    Initial Commit

---
+++ SwiftUIFirstApp/SwiftUIFirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFirstAppApp.swift
+//  SwiftUIFirstApp
+//
+//  Created by Sonu Martin on 02/09/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
